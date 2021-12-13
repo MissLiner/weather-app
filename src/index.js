2bf9fc13631f0dc73777a081dcabc33b3c0c7b12
MissLiner <miss.liner@yahoo.com>
2021-12-13 16:19:42 -0500

    Add functionality to print weather in  console

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import './style.css';
+
+const geoForm = document.getElementById('geo-form');
+const cityInput = document.getElementById('city');
+const stateInput = document.getElementById('state');
+const submitBtn = document.getElementById('btn-submit');
+let cityState = 'new+york,US-NY';
+const weatherOutput = document.getElementById('weather-output');
+
+// "api.openweathermap.org/data/2.5/weather?q=new+york,US-NY&appid=d2d96f2512c3f3f99ab4627f4c42e945";
+
+function transformLocationData() {
+  let city = cityInput.value;
+  city = city.replace(/\s/g, '+');
+  city = city.toLowerCase();
+  let state = stateInput.value;
+  state = 'US-' + state.toUpperCase();
+  cityState = city + ',' + state;
+  console.log(cityState);
+}
+
+(async function getWeather() {
+  transformLocationData();
+  const weatherLink = `https://api.openweathermap.org/data/2.5/weather?q=${cityState}&appid=d2d96f2512c3f3f99ab4627f4c42e945`;
+
+  let response = await fetch(weatherLink, { mode: 'cors' });
+  let responseJSON = await response.json();
+  console.log(responseJSON);
+  //weatherOutput.src = 
+})()
+
+submitBtn.addEventListener('click', () => {
+  getWeather();
+})
